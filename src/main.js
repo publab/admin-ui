fa471561a199f509f3acd22f1f3f6c19234d8db8
duanzhiwei <89340545@qq.com>
2019-11-26 18:12:00 +0800

    'ant'

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './plugins/ant-design-vue.js';
 Vue.config.productionTip = false;
 new Vue({
     router,
     store,
     render: h => h(App)
 }).$mount('#app');
+//移动端UI框架 vant
 //# sourceMappingURL=main.js.map
