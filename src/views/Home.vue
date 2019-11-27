d5e088a7b525da8eb9b558682c7de8fa8a2c26d1
duanzhiwei <89340545@qq.com>
2019-11-27 18:18:46 +0800

    'nav'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,53 +2,68 @@
     <a-layout id="components-layout-demo-side" style="min-height: 100vh">
         <a-layout-sider collapsible v-model="collapsed">
             <div class="logo" />
-            <a-menu theme="dark" :defaultSelectedKeys="['1']" mode="inline">
-                <a-menu-item key="1">
-                    <a-icon type="pie-chart" />
-                    <span>Option 1</span>
-                </a-menu-item>
-                <a-menu-item key="2">
-                    <a-icon type="desktop" />
-                    <span>Option 2</span>
-                </a-menu-item>
-                <a-sub-menu key="sub1">
-                    <span slot="title"><a-icon type="user" /><span>User</span></span>
-                    <a-menu-item key="3">Tom</a-menu-item>
-                    <a-menu-item key="4">Bill</a-menu-item>
-                    <a-menu-item key="5">Alex</a-menu-item>
+
+            <a-menu
+                    theme="dark"
+                    @click="handleClick"
+                    :defaultSelectedKeys="defaultSelected"
+                    mode="inline"
+                    :inlineCollapsed="collapsed"
+            >
+                <a-sub-menu key="sub1" @titleClick="titleClick">
+                    <span slot="title"><a-icon type="mail" /><span>Navigation One</span></span>
+                    <a-menu-item-group key="g1">
+                        <template slot="title"
+                        ><a-icon type="qq" /><span>Item 1</span></template
+                        >
+                        <a-menu-item key="1"><router-link to="nav">nav</router-link></a-menu-item>
+                        <a-menu-item key="2">Option 2</a-menu-item>
+                    </a-menu-item-group>
+                    <a-menu-item-group key="g2" title="Item 2">
+                        <a-menu-item key="3">Option 3</a-menu-item>
+                        <a-menu-item key="4">Option 4</a-menu-item>
+                    </a-menu-item-group>
                 </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <span slot="title"><a-icon type="team" /><span>Team</span></span>
-                    <a-menu-item key="6">Team 1</a-menu-item>
-                    <a-menu-item key="8">Team 2</a-menu-item>
+                <a-sub-menu key="sub2" @titleClick="titleClick">
+                    <span slot="title"><a-icon type="appstore" /><span>Navigation Two</span></span>
+                    <a-menu-item key="5">Option 5</a-menu-item>
+                    <a-menu-item key="6">Option 6</a-menu-item>
+                    <a-sub-menu key="sub3" title="Submenu">
+                        <a-menu-item key="7">Option 7</a-menu-item>
+                        <a-menu-item key="8">Option 8</a-menu-item>
+                    </a-sub-menu>
                 </a-sub-menu>
-                <a-menu-item key="9">
-                    <a-icon type="file" />
-                    <span>File</span>
-                </a-menu-item>
+                <a-sub-menu key="sub4">
+                    <span slot="title"><a-icon type="setting" /><span>Navigation Three</span></span>
+                    <a-menu-item key="9">Option 9</a-menu-item>
+                    <a-menu-item key="10">Option 10</a-menu-item>
+                    <a-menu-item key="11">Option 11</a-menu-item>
+                    <a-menu-item key="12">Option 12</a-menu-item>
+                </a-sub-menu>
             </a-menu>
+
+
         </a-layout-sider>
         <a-layout>
-            <a-layout-header style="background: #fff; padding: 0">
+            <a-layout-header >
                 <a-row>
-                    <a-col :span="3">
-
-                    </a-col>
-                    <a-col :span="19">
+                    <a-col :span="3"></a-col>
+                    <a-col :span="18">
                         <a-menu
-                                theme="light"
+                                theme="dark"
                                 mode="horizontal"
                                 :defaultSelectedKeys="['2']"
                                 :style="{ lineHeight: '64px' }"
+                                @select="topSelect"
                         >
                             <a-menu-item key="1"><router-link to="/login">login</router-link></a-menu-item>
-                            <a-menu-item key="2">nav 2</a-menu-item>
+                            <a-menu-item key="2"><router-link to="/">首页</router-link></a-menu-item>
                             <a-menu-item key="3">nav 3</a-menu-item>
                         </a-menu>
                     </a-col>
-                    <a-col :span="2">
-                        <a-dropdown>
-                            <a class="ant-dropdown-link" href="#"> Hover me <a-icon type="down" /> </a>
+                    <a-col :span="3" :style="{textAlign: 'right'}">
+                        <a-dropdown placement="bottomRight">
+                            <a class="ant-dropdown-link" :style="{color: '#fff'}"> Hover me <a-icon type="down" /> </a>
                             <a-menu slot="overlay">
                                 <a-menu-item key="0">
                                     <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
@@ -79,11 +94,16 @@
     export default {
         data() {
             return {
-                collapsed: false,
-                openKeys: ['sub1'],
+                collapsed: true,
+                defaultSelected: [
+                    // "1"
+                ]
             };
         },
         methods: {
+            topSelect(){
+                window.console.log(1111);
+            },
             handleClick(e) {
                 window.console.log('click', e);
             },
@@ -92,9 +112,7 @@
             },
         },
         watch: {
-            openKeys(val) {
-                window.console.log('openKeys', val);
-            },
+
         },
         components: {
 
