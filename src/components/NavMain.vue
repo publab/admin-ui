770286a10d1b03f9a2148f0489872f252aea1845
duanzhiwei <89340545@qq.com>
2019-11-27 10:32:56 +0800

    'transition'

---
+++ src/components/NavMain.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button v-on:click="show = !show">
+            Toggle
+        </button>
+        <transition name="fade" :duration="1000">
+            <p v-if="show">hello</p>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                show: true
+            }
+        }
+    }
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import NavMain from '../components/NavMain.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +9,22 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    children:[
+        {
+            path: 'nav',
+            name: 'nav',
+            component: NavMain,
+        }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   }
 ]
 
