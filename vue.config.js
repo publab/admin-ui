2a3efe0965df19f05290a13574bc8542a5c88689
duanzhiwei <89340545@qq.com>
2019-11-29 13:21:49 +0800

    'gz-file'

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 // vue.config.js
+
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
     publicPath: '/',
     productionSourceMap: false,
+    configureWebpack: {
+        plugins: [
+            new CompressionPlugin({
+                test:/\.js$|\.html$|\.css/,
+                threshold:10240,//
+                deleteOriginalAssets:false,
+            })
+        ]
+    }
 }
