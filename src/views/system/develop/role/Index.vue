028d627c9cec9427b7c79260f20b2a8cad2c022c
duanzhiwei <89340545@qq.com>
2020-03-06 17:10:42 +0800

    'bak'

--- src/views/system/develop/role/Index.vue
+++ src/views/system/develop/role/Index.vue
@@ -1,60 +1,91 @@
 <template>
-    <a-table :columns="columns" :dataSource="data" :pagination="pagination" @change="handleTableChange" rowKey="id">
-        <template slot="title">
-            <a-row type="flex" justify="space-around" align="middle">
-                <a-col :span="18">角色列表</a-col>
-                <a-col :span="6" :style="{ textAlign:'right'}">
-                    <a-button type="primary" icon="plus" @click="jump('/system/develop/role/create')">添加角色</a-button>
-                </a-col>
-            </a-row>
-        </template>
-        <div
-                slot="filterDropdown"
-                slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
-                style="padding: 8px"
-        >
-            <a-input
-                    v-ant-ref="c => searchInput = c"
-                    :placeholder="`Search ${column.dataIndex}`"
-                    :value="selectedKeys[0]"
-                    @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
-                    @pressEnter="() => handleSearch(selectedKeys, confirm)"
-                    style="width: 188px; margin-bottom: 8px; display: block;"
+    <div>
+        <a-table :columns="columns" :dataSource="data" :pagination="pagination" @change="handleTableChange" rowKey="id">
+            <template slot="title">
+                <a-row type="flex" justify="space-around" align="middle">
+                    <a-col :span="18">角色列表</a-col>
+                    <a-col :span="6" :style="{ textAlign:'right'}">
+                        <a-button type="primary" icon="plus" @click="jump('/system/develop/role/create')">添加角色</a-button>
+                    </a-col>
+                </a-row>
+                <a-form layout="inline" :form="form">
+                    <a-form-item label="Field A">
+                        <a-input
+                                v-decorator="[
+                          'gender',
+                        ]"
+                                placeholder="input Field A"
+                        />
+                    </a-form-item>
+                    <a-form-item label="Field B">
+                        <a-input
+                                v-decorator="[
+                          'sex',
+                        ]"
+                                placeholder="input Field B"
+                        />
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button type="primary" html-type="submit">
+                            Search
+                        </a-button>
+                        <a-button :style="{ marginLeft: '8px' }" @click="() => this.form.resetFields()">
+                            Clear
+                        </a-button>
+                    </a-form-item>
+                </a-form>
+            </template>
+            <div
+                    slot="filterDropdown"
+                    slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }"
+                    style="padding: 8px"
+            >
+                <a-input
+                        v-ant-ref="c => searchInput = c"
+                        :placeholder="`Search ${column.dataIndex}`"
+                        :value="selectedKeys[0]"
+                        @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+                        @pressEnter="() => handleSearch(selectedKeys, confirm)"
+                        style="width: 188px; margin-bottom: 8px; display: block;"
+                />
+                <a-button
+                        type="primary"
+                        @click="() => handleSearch(selectedKeys, confirm)"
+                        icon="search"
+                        size="small"
+                        style="width: 90px; margin-right: 8px"
+                >Search</a-button
+                >
+                <a-button @click="clearFilters" size="small" style="width: 90px"
+                >Reset</a-button
+                >
+            </div>
+            <a-icon
+                    slot="filterIcon"
+                    slot-scope="filtered"
+                    type="search"
+                    :style="{ color: filtered ? '#108ee9' : undefined }"
             />
-            <a-button
-                    type="primary"
-                    @click="() => handleSearch(selectedKeys, confirm)"
-                    icon="search"
-                    size="small"
-                    style="width: 90px; margin-right: 8px"
-            >Search</a-button
-            >
-            <a-button @click="() => handleReset(clearFilters)" size="small" style="width: 90px"
-            >Reset</a-button
-            >
-        </div>
-        <a-icon
-                slot="filterIcon"
-                type="search"
-        />
-        <template slot="is_work" slot-scope="data">
-            {{data == 1 ? '正常' : '停止'}}
-        </template>
-        <template slot="operation" slot-scope="data, record">
-            <a @click="jump('/system/develop/role/update/'+record.id)">编辑</a>
-            <a-divider type="vertical" />
-            <a-popconfirm
-                title="Sure to delete?"
-                @confirm="() => onDelete(record.id)">
-                <a>删除</a>
-            </a-popconfirm>
-        </template>
-    </a-table>
+            <template slot="is_work" slot-scope="data">
+                {{data == 1 ? '正常' : '停止'}}
+            </template>
+            <template slot="operation" slot-scope="data, record">
+                <a @click="jump('/system/develop/role/update/'+record.id)">编辑</a>
+                <a-divider type="vertical" />
+                <a-popconfirm
+                    title="Sure to delete?"
+                    @confirm="() => onDelete(record.id)">
+                    <a>删除</a>
+                </a-popconfirm>
+            </template>
+        </a-table>
+    </div>
 </template>
 <script>
     export default {
         data() {
             return {
+                form: this.$form.createForm(this),
                 data: [],
                 pagination: {},
                 filteredInfo: null,
@@ -101,6 +132,10 @@
                     }
                 });
             },
+            handleSearch(selectedKeys, confirm) {
+                confirm();
+                this.searchText = selectedKeys[0];
+            },
             onDelete(id){
                 let _this = this;
                 axios.post('system/develop/role/delete/'+id,{}).then((response) => {
@@ -111,10 +146,6 @@
                     _this.data = _this.data.filter(item => item.id !== id);
 
                 });
-            },
-            handleReset(clearFilters) {
-                clearFilters();
-                this.searchText = '';
             },
         }
     }
