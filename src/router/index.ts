28366050a4a0d8dd661b6b3831c8cf4cd8f51d94
duanzhiwei <89340545@qq.com>
2020-03-15 08:00:40 +0800

    'bak'

--- src/router/index.ts
+++ src/router/index.ts
@@ -43,40 +43,48 @@ export const asyncRouterMap = [
             {
                 path: 'index',
                 meta: { title: '仪表盘', keepAlive: true},
-                component: () => import('../views/Welcome.vue'),
+                component: tree(() => import('../views/Welcome.vue')),
             },
             {
                 path: 'system',
+                meta: { title: '系统'},
                 component: tree(),
                 children: [
                     {
                         path: 'develop',
+                        meta: { title: '开发管理'},
                         component: tree(),
                         children:[
                             {
                                 path: 'permission',
+                                meta: { title: '权限列表', keepAlive: true},
                                 component: tree(() => import('../views/system/develop/permission/Index.vue')),
                                 children:[
                                     {
                                         path: 'create',
+                                        meta: { title: '创建'},
                                         component: tree(() => import('../views/system/develop/permission/Create.vue'))
                                     },
                                     {
                                         path: 'update/:id',
+                                        meta: { title: '更新'},
                                         component: tree(() => import('../views/system/develop/permission/Create.vue'))
                                     },
                                 ]
                             },
                             {
                                 path: 'role',
+                                meta: { title: '角色列表', keepAlive: true},
                                 component: tree(() => import('../views/system/develop/role/Index.vue')),
                                 children:[
                                     {
                                         path: 'create',
+                                        meta: { title: '创建'},
                                         component: tree(() => import('../views/system/develop/role/Create.vue'))
                                     },
                                     {
                                         path: 'update/:id',
+                                        meta: { title: '更新'},
                                         component: tree(() => import('../views/system/develop/role/Create.vue'))
                                     },
                                 ]
