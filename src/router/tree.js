6ca088dc376063579376bb030f9eb892759393c7
duanzhiwei <89340545@qq.com>
2020-03-18 10:33:01 +0800

    'bak'

--- src/router/tree.js
+++ src/router/tree.js
@@ -1,6 +1,6 @@
 const Tree = function tree(component = false) {
     return {
-        name: 'tree',
+        name: 'Tree',
         computed: {
             showChild() {
                 const deepestMatchedRoute = this.$route.matched[this.$route.matched.length - 1];
@@ -21,37 +21,6 @@ const Tree = function tree(component = false) {
     };
 }
 
-const RouterView = {
-    name: 'RouteView',
-    props: {
-        keepAlive: {
-            type: Boolean,
-            default: true
-        }
-    },
-    data () {
-        return {}
-    },
-    render () {
-        const { $route: { meta }, $store: { getters } } = this
-        const inKeep = (
-            <keep-alive>
-                <router-view />
-            </keep-alive>
-        )
-        const notKeep = (
-            <router-view />
-        )
-        // 这里增加了 multiTab 的判断，当开启了 multiTab 时
-        // 应当全部组件皆缓存，否则会导致切换页面后页面还原成原始状态
-        // 若确实不需要，可改为 return meta.keepAlive ? inKeep : notKeep
-        if (!getters.multiTab && !meta.keepAlive) {
-            return notKeep
-        }
-        return this.keepAlive || getters.multiTab || meta.keepAlive ? inKeep : notKeep
-    }
-}
-
 export {
-    Tree,RouterView
+    Tree
 }
