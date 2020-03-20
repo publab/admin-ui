882c74abfae69bc27809daeea6afe912455570ec
duanzhiwei <89340545@qq.com>
2020-03-20 23:11:49 +0800

    'bak'

--- src/components/Nav/Top.vue
+++ src/components/Nav/Top.vue
@@ -5,39 +5,23 @@
                 <a-icon class="trigger" :type="collapsed ? 'menu-unfold' : 'menu-fold'" @click="toggle"/>
             </a-col>
             <a-col :span="20" :style="{textAlign: 'right'}">
-                <a target="_blank" class="action">
-                    <span class="action">
-                      <a-icon type="question-circle-o"></a-icon>
-                    </span>
-                </a>
-                <notice-icon class="action"></notice-icon>
-                <a-dropdown placement="bottomRight" :trigger="['click','hover']">
-                    <span class="action">
-                      <a-avatar class="avatar" size="small" :src="avatar"/>
-                      <span class="ant-dropdown-link">{{ name }} <a-icon type="down" /></span>
-                    </span>
-                    <a-menu slot="overlay" style="width: 150px">
-                        <a-menu-item key="11"><a-icon type="user" />个人中心</a-menu-item>
-                        <a-menu-item key="21"><a-icon type="setting" />账户设置</a-menu-item>
-                        <a-menu-item key="41" disabled><a-icon type="setting" />禁用</a-menu-item>
-                        <a-menu-divider/>
-                        <a-menu-item key="31" @click="logout"><a-icon type="logout"/>退出登录</a-menu-item>
-                    </a-menu>
-                </a-dropdown>
+                <user-menu></user-menu>
             </a-col>
         </a-row>
     </a-layout-header>
 </template>
 
 <script>
-    import NoticeIcon from '@/components/NoticeIcon'
-    import { mapState } from 'vuex'
+    import UserMenu from '@/components/Tool/UserMenu'
 
     export default {
         data() {
             return {
 
             };
+        },
+        components: {
+            UserMenu
         },
         props: {
             collapsed: {
@@ -46,42 +30,7 @@
                 default: false
             },
         },
-        computed: {
-            ...mapState({
-                name: state => state.user.name,
-                avatar: state => state.user.avatar
-            })
-        },
-        components: {
-            NoticeIcon
-        },
-        created (){
-            this.$store.dispatch('user/GetInfo',this)
-        },
         methods: {
-            logout(){
-
-                var _this = this;
-
-                this.$confirm({
-                    title: '提示',
-                    content: '真的要注销登录吗 ?',
-                    onOk() {
-                        axios.post('logout').then((response) => {
-                            if(!response.status){
-                                return _this.$message.error(response.message);
-                            }
-                            sessionStorage.clear()
-                            _this.jump('/user/login');
-                        });
-                    },
-                    onCancel() {
-
-                    },
-                });
-
-
-            },
             toggle () {
                 this.$emit('toggle')
             }
@@ -98,33 +47,5 @@
 
     .trigger:hover {
         color: #1890ff;
-    }
-</style>
-<style lang="less">
-    .action {
-        cursor: pointer;
-        padding: 0 12px;
-        display: inline-block;
-        transition: all 0.3s;
-        height: 100%;
-        color: rgba(0, 0, 0, 0.65);
-
-        &:hover {
-            background: rgba(0, 0, 0, 0.025);
-        }
-
-        .avatar {
-            margin: 20px 8px 20px 0;
-            color: #1890ff;
-            background: hsla(0, 0%, 100%, 0.85);
-            vertical-align: middle;
-            position: relative;
-            top: -3px;
-        }
-
-        .icon {
-            font-size: 16px;
-            padding: 4px;
-        }
     }
 </style>
