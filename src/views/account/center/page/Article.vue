6e6973a154d16b13981cd87c3ae42fcfadc5c44a
duanzhiwei <89340545@qq.com>
2020-03-24 06:55:12 +0800

    'bak'

--- src/views/account/center/page/Article.vue
+++ src/views/account/center/page/Article.vue
@@ -53,92 +53,92 @@
 <script>
 
 export default {
-  name: 'Article',
-  components: {
+    name: 'Article',
+    components: {
 
-  },
-  data () {
-    return {
-      loading: true,
-      loadingMore: true,
-      data: [
-          {
-              "id":1,
-              "avatar":"https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png",
-              "owner":"周星星",
-              "content":"段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。",
-              "star":552,
-              "percent":519,
-              "like":907,
-              "message":501,
-              "description":"在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
-              "href":"https://ant.design",
-              "title":"Alipay",
-              "updatedAt":"1981-01-21 22:47:23",
-              "members":[
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png",
-                      "name":"曲丽丽",
-                      "id":"member1"
-                  },
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png",
-                      "name":"王昭君",
-                      "id":"member2"
-                  },
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png",
-                      "name":"董娜娜",
-                      "id":"member3"
-                  }
-              ],
-              "activeUser":179870,
-              "newUser":1975,
-              "cover":"https://gw.alipayobjects.com/zos/rmsportal/uMfMFlvUuceEyPpotzlq.png"
-          },
-          {
-              "id":2,
-              "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png",
-              "owner":"林东东",
-              "content":"段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。",
-              "star":539,
-              "percent":817,
-              "like":923,
-              "message":268,
-              "description":"在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
-              "href":"https://ant.design",
-              "title":"Angular",
-              "updatedAt":"1998-07-03 15:08:45",
-              "members":[
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png",
-                      "name":"曲丽丽",
-                      "id":"member1"
-                  },
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png",
-                      "name":"王昭君",
-                      "id":"member2"
-                  },
-                  {
-                      "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png",
-                      "name":"董娜娜",
-                      "id":"member3"
-                  }
-              ],
-              "activeUser":141976,
-              "newUser":1539,
-              "cover":"https://gw.alipayobjects.com/zos/rmsportal/iZBVOIhGJiAnhplqjvZW.png"
-          },
-      ]
+    },
+    data () {
+        return {
+          loading: true,
+          loadingMore: true,
+          data: [
+              {
+                  "id":1,
+                  "avatar":"https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png",
+                  "owner":"周星星",
+                  "content":"段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。",
+                  "star":552,
+                  "percent":519,
+                  "like":907,
+                  "message":501,
+                  "description":"在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
+                  "href":"https://ant.design",
+                  "title":"Alipay",
+                  "updatedAt":"1981-01-21 22:47:23",
+                  "members":[
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png",
+                          "name":"曲丽丽",
+                          "id":"member1"
+                      },
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png",
+                          "name":"王昭君",
+                          "id":"member2"
+                      },
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png",
+                          "name":"董娜娜",
+                          "id":"member3"
+                      }
+                  ],
+                  "activeUser":179870,
+                  "newUser":1975,
+                  "cover":"https://gw.alipayobjects.com/zos/rmsportal/uMfMFlvUuceEyPpotzlq.png"
+              },
+              {
+                  "id":2,
+                  "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png",
+                  "owner":"林东东",
+                  "content":"段落示意：蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。蚂蚁金服设计平台 ant.design，用最小的工作量，无缝接入蚂蚁金服生态，提供跨越设计与开发的体验解决方案。",
+                  "star":539,
+                  "percent":817,
+                  "like":923,
+                  "message":268,
+                  "description":"在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。",
+                  "href":"https://ant.design",
+                  "title":"Angular",
+                  "updatedAt":"1998-07-03 15:08:45",
+                  "members":[
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/ZiESqWwCXBRQoaPONSJe.png",
+                          "name":"曲丽丽",
+                          "id":"member1"
+                      },
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/tBOxZPlITHqwlGjsJWaF.png",
+                          "name":"王昭君",
+                          "id":"member2"
+                      },
+                      {
+                          "avatar":"https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png",
+                          "name":"董娜娜",
+                          "id":"member3"
+                      }
+                  ],
+                  "activeUser":141976,
+                  "newUser":1539,
+                  "cover":"https://gw.alipayobjects.com/zos/rmsportal/iZBVOIhGJiAnhplqjvZW.png"
+              },
+          ]
+        }
+    },
+    mounted () {
+        setTimeout(()=> this.loading = false,1000);
+    },
+    methods: {
+
     }
-  },
-  mounted () {
-      setTimeout(()=> this.loading = false,1000);
-  },
-  methods: {
-
-  }
 }
 </script>
 <style lang="less" scoped>
